fe130d386105a43378c3edb76239dd8087b7b1b1
Olivier Nguyen <olivier_nguyen@live.fr>
2017-11-13 00:06:06 +0100

    add missing $ in alerts-list url

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from api.views.alerts import AlertsList, AlertsAPI
 
 urlpatterns = [
     url(r'^bitcoin/$', BitcoinAPI.as_view(), name="bitcoin"),
-    url(r'^alerts/', AlertsList.as_view(), name="alerts-list"),
+    url(r'^alerts/$', AlertsList.as_view(), name="alerts-list"),
     url(r'^alerts/(?P<pk>[0-9]+)/$', AlertsAPI.as_view(), name="alerts-api"),
 ]
