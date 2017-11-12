fb819e379d90462213674ed9f38a5eb220871913
Olivier Nguyen <olivier_nguyen@live.fr>
2017-11-12 22:56:29 +0100

    create Alerts model

---
+++ alerts/enums/operator.py
@@ -0,0 +1,13 @@
+EQ = 'EQ',
+LT = 'LT',
+LTE = 'LTE',
+GT = 'GT',
+GTE = 'GTE',
+
+OPERATOR = (
+    (EQ, 'Equals'),
+    (LT, 'Lower than'),
+    (LTE, 'Lower or equal than'),
+    (GT, 'Greater than'),
+    (GTE, 'Greater or equal than'),
+)
--- alerts/models.py
+++ alerts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from .enums.operator import OPERATOR
+
 
 class Alerts(models.Model):
     price = models.IntegerField(max_length=30)
-    operatir = models.
+    operator = models.CharField(choices=OPERATOR, max_length=3)
+    sent_at = models.DateField()
     created_at = models.DateField(auto_now_add=True)
