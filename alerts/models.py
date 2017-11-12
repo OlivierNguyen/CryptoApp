15524a78d74c9760e35fd526899eb59c39101089
Olivier Nguyen <olivier_nguyen@live.fr>
2017-11-12 23:33:10 +0100

    add created_by field to Alerts model

--- alerts/models.py
+++ alerts/models.py
@@ -6,4 +6,5 @@ class Alerts(models.Model):
     price = models.IntegerField(max_length=30)
     operator = models.CharField(choices=OPERATOR, max_length=3)
     sent_at = models.DateField()
+    created_by = models.EmailField(default='')
     created_at = models.DateField(auto_now_add=True)
